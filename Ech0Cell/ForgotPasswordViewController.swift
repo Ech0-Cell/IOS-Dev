ff849e9e35896c34ac4a0f1f125181b70e1b2209
Burak Kağan Taşkıran <burakkagan29@gmail.com>
2024-06-08 09:11:20 +0300

    Create ForgotPasswordViewController.swift

---
+++ Ech0Cell/ForgotPasswordViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var smsVerificationTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
