b1e5272a98d1171edc7a1fa57cbaa8a403f8d85a
Burak Kağan Taşkıran <burakkagan29@gmail.com>
2024-06-08 09:11:21 +0300

    Create BuyPackagesViewController.swift

---
+++ Ech0Cell/BuyPackagesViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class BuyPackagesViewController: UIViewController {
+    
+    @IBOutlet weak var package1Button: UIButton!
+    @IBOutlet weak var package2Button: UIButton!
+    @IBOutlet weak var package3Button: UIButton!
+    @IBOutlet weak var discountCode: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
